52b000dd0cbbbd1653a869863fe5b8d1c0869d94
Robert Zondervan <rjzondervan@gmail.com>
2020-04-21 20:00:16 +0200

    up

--- api/src/DataFixtures/AppFixtures.php
+++ api/src/DataFixtures/AppFixtures.php
@@ -17,14 +17,14 @@ use PhpOffice\PhpSpreadsheet;
 
 class AppFixtures extends Fixture
 {
-    private $params;
-    private $encoder;
-
-    public function __construct(ParameterBagInterface $params, UserPasswordEncoderInterface $encoder)
-    {
-        $this->params = $params;
-        $this->encoder = $encoder;
-    }
+//    private $params;
+//    private $encoder;
+//
+//    public function __construct(UserPasswordEncoderInterface $encoder)
+//    {
+////        $this->params = $params;
+//        $this->encoder = $encoder;
+//    }
 
     public function load(ObjectManager $manager)
     {
